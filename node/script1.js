d63d0ce2a0d11b469c89557991a1a278c8affefe
Redyf <mateusalvespereira7@gmail.com>
2023-08-29 19:45:29 -0300

    New exercises for node

--- node/script1.js
+++ node/script1.js
@@ -1,7 +1,13 @@
 // const c = require("./script2")
-import { largeNumber } from "./script2.js"
+// import { largeNumber } from "./script2.js"
 
-const a = largeNumber
+// ES2022: Await import 
+let a;
+if (1 === 1) {
+  const { largeNumber } = await import("./script2.js")
+  a = largeNumber;
+}
+
 const b = 5;
 
-console.log(a + b)
+console.log(a, b)
